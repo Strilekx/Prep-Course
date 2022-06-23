e8f345964c36d6e04a7ed75daf7919124ad5f1c4
Strilekx <104595806+Strilekx@users.noreply.github.com>
2022-06-23 12:22:20 -0300

    agregando ando

--- homework/homework.js
+++ homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Hola me llamo emanuell";
+const nuevaString = "probando ando";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 2;
